c512eca1737c67464d5c6eeef1942013a22f27b0
Amr M. Taha <83893565+3mrotaha@users.noreply.github.com>
2023-02-06 01:07:54 +0200

    Update EXINT_int.h

--- On-demand Traffic light control/MCAL/EX_Interrupt/EXINT_int.h
+++ On-demand Traffic light control/MCAL/EX_Interrupt/EXINT_int.h
@@ -21,41 +21,41 @@
 #define EXINTuint8_INT2					2
 
 /*********************************************************
-** Function Name: EXINT_enuInit							**
-** Return Type: ErrorStates_t							**
-** Arguments: None										**
-** Functionality: Initializes the External Interrupt	**
-**				module.									**
+** Function Name: EXINT_enuInit                         **
+** Return Type: ErrorStates_t                           **
+** Arguments: None                                      **
+** Functionality: Initializes the External Interrupt    **
+**               module.                                **
 *********************************************************/
 ErrorStates_t EXINT_enuInit(void);
 
 /*********************************************************
-** Function Name: EXINT_enuIntEnable					**
-** Return Type: ErrorStates_t							**
-** Arguments: uint8 Copy_uint8IntID,					** 
-**			  uint8 Copy_uint8TriggerType				**
+** Function Name: EXINT_enuIntEnable                    **
+** Return Type: ErrorStates_t                           **
+** Arguments: uint8 Copy_uint8IntID,                    ** 
+**         uint8 Copy_uint8TriggerType                  **
 ** Functionality: Enables a specific External Interrupt **
-**				and sets its trigger type.				**
+**         and sets its trigger type.                   **
 *********************************************************/
 ErrorStates_t EXINT_enuIntEnable(uint8 Copy_uint8IntID, uint8 Copy_uint8TriggerType);
 
 /*********************************************************
-** Function Name: EXINT_enuIntDisable					**
-** Return Type: ErrorStates_t							**
-** Arguments: uint8 Copy_uint8IntID						**
+** Function Name: EXINT_enuIntDisable                   **
+** Return Type: ErrorStates_t                           **
+** Arguments: uint8 Copy_uint8IntID                     **
 ** Functionality: Disables a specific External Interrupt**
 *********************************************************/
 ErrorStates_t EXINT_enuIntDisable(uint8 Copy_uint8IntID);
 
 /*********************************************************
-** Function Name: EXINT_enuCallBack						**
-** Return Type: ErrorStates_t							**
-** Arguments: void (Copy_pvoidfunCall)(void),			**
-**			  void *Copy_pvoidParameter,				**
-**			  uint8 Copy_uint8IntID						**
-** Functionality: Registers a callback function to be	**
-**		 executed when a specific External Interrupt is **
-**		 triggered.										**
+** Function Name: EXINT_enuCallBack                     **
+** Return Type: ErrorStates_t                           **
+** Arguments: void (Copy_pvoidfunCall)(void),           **
+**        void *Copy_pvoidParameter,                    **
+**       uint8 Copy_uint8IntID                          **
+** Functionality: Registers a callback function to be   **
+**    executed when a specific External Interrupt is    **
+**    triggered.                                        **
 *********************************************************/
 ErrorStates_t EXINT_enuCallBack(void (*Copy_pvoidfunCall)(void*), void *Copy_pvoidParameter, uint8 Copy_uint8IntID);
 
