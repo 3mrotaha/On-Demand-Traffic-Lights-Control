1b96ec6a0a7077934cbfdd421b6d310c48c8397f
3mrotaha <tamr4512@gmail.com>
2023-02-11 19:59:51 +0200

    user stories updated

--- On-demand Traffic light control/main.c
+++ On-demand Traffic light control/main.c
@@ -5,20 +5,13 @@
  * Author : amrmo
  */ 
 
-#include <avr/io.h>
-#include <util/delay.h>
+
 #include "LIB/std_types.h"
 #include "LIB/ErrorStates.h"
-#include "MCAL/DIO/DIO_int.h"
 
 int main(void)
 {
-	// set pin direction in PORTA to output
-	DIO_enuSetPinDir(DIO_uint8_PORTA, DIO_uint8_PIN0, INPUT);
-	
-	// get the value on the pin 0 of port A
-	uint8 value; // a variable to store the value of the pin
-	DIO_enuGetPinValue(DIO_uint8_PORTA, DIO_uint8_PIN0, &value);
+	App_enuStart();
 	while(1){
 
 	}
