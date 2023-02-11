5f5ffb0da21eb3f39d2edf3e6103a62953f78553
3mrotaha <tamr4512@gmail.com>
2023-02-12 00:57:30 +0200

    added driver testing videos

--- On-demand Traffic light control/Debug/main.d
+++ On-demand Traffic light control/Debug/main.d
@@ -1,5 +1,29 @@
-main.d main.o: .././main.c .././LIB/std_types.h .././LIB/ErrorStates.h
+main.d main.o: .././main.c \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h \
+ .././LIB/std_types.h .././LIB/ErrorStates.h \
+ .././MCAL/EX_Interrupt/EXINT_int.h .././MCAL/DIO/DIO_int.h
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h:
 
 .././LIB/std_types.h:
 
 .././LIB/ErrorStates.h:
+
+.././MCAL/EX_Interrupt/EXINT_int.h:
+
+.././MCAL/DIO/DIO_int.h:
--- On-demand Traffic light control/main.c
+++ On-demand Traffic light control/main.c
@@ -5,13 +5,26 @@
  * Author : amrmo
  */ 
 
-
+#include <util/delay.h>
 #include "LIB/std_types.h"
 #include "LIB/ErrorStates.h"
+#include "MCAL/EX_Interrupt/EXINT_int.h"
+#include "MCAL/DIO/DIO_int.h"
+void togglePin(void){
+	DIO_enuTogPinValue(DIO_uint8_PORTA, DIO_uint8_PIN0);
+}
 
 int main(void)
 {
-	App_enuStart();
+	// initialize the interrupt driver
+	EXINT_enuInit();
+	
+	// call back a function
+	EXINT_enuCallBack(&togglePin, NULL, EXINTuint8_INT0);
+	
+	// initailize the pin
+	DIO_enuSetPinDir(DIO_uint8_PORTA, DIO_uint8_PIN0, OUTPUT);
+	DIO_enuSetPinValue(DIO_uint8_PORTA, DIO_uint8_PIN0, HIGH);
 	while(1){
 
 	}
